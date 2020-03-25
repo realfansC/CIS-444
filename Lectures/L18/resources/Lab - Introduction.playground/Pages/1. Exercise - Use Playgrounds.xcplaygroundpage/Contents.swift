397cdf3e86390a7df0823ea4b4fb40cd6a67da21
Rebecca Slatkin <rslatkin@junelife.com>
2020-03-25 11:43:10 -0400

    Lecture 19 assets

--- Lectures/L18/resources/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Lectures/L18/resources/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -7,8 +7,8 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
-
+print("hi my name is")
+print("dog is my friend")
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
---
+++ Lectures/L19/lecture.md
@@ -0,0 +1,16 @@
+# CIS-444
+ Lecture 19
+ March 25, 2020
+
+
+
+
+### Due and submitted to me by end of class (`rcslatki@syr.edu`):
+* [ ] Create an app that displays distinct types of information about yourself in separate tabs.
+
+#### Example:
+
+![inline]()
+![inline]()
+![inline()]
+
